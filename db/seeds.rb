42d5374648be9ae28ebd80b645f2ce32af3ac40d
Gabriel <gabs1995@hotmail.com>
2022-01-19 17:04:43 -0500

    Check

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.find_each(&:destroy)
+
+Restaurant.create(name: "Epicure", address: "75008 Paris", category: "french")
+Restaurant.create(name: "La Benita", address: "25363 Characato", category: "chinese")
+Restaurant.create(name: "Los Guisos", address: "09373 Lambramani", category: "italian")
+Restaurant.create(name: "La Tradición", address: "56378 Bustamante", category: "japanese")
+Restaurant.create(name: "Rústica", address: "76528 Socabaya", category: "french")
+
+puts 'Your database is populated now!'
